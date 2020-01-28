7402a198bb16f812c4beef4c1c12dfdb8625e121
Issa Mouawad <issa.mouawad@outlook.com>
2020-01-28 16:57:34 +0100

    v

--- detectron2/modeling/meta_arch/rcnn.py
+++ detectron2/modeling/meta_arch/rcnn.py
@@ -171,8 +171,9 @@ class VideoRCNN(nn.Module):
                 height = input_per_image.get("height", image_size[0])
                 width = input_per_image.get("width", image_size[1])
                 r = detector_postprocess(results_per_image, height, width)
-                processed_results.append({"instances": r})
-            return processed_results
+                self.tracker.track(r, None,None)
+                return self.tracker.get_display_tracks()
+                
             processed_results = []
             for results_per_image, input_per_image, image_size in zip(
                 results, batched_inputs, images.image_sizes
@@ -183,8 +184,7 @@ class VideoRCNN(nn.Module):
                 processed_results.append({"instances": r})
                 
                     #print('another time, currently there are %d tracks'%len(self.tracker.tracks))
-                self.tracker.track(r, None,None)
-                return self.tracker.get_display_tracks()
+                
                 
                     
                 
