d29d41b96d2900e80179697de8834d36a4d43a57
issamouawad <issa.mouawad@gmail.com>
2020-07-02 18:06:30 +0200

    ss

--- tracker_validation.py
+++ tracker_validation.py
@@ -232,7 +232,7 @@ for setting in settings:
       out_tracking = cv2.VideoWriter('debug_kitti.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 30, dump_image.shape[1::-1])
       predictor = DefaultPredictor(cfg)
       #prop_limit=60
-      predictor.model.tracker = SoftTracker()
+      predictor.model.tracker = Tracker()
       print(folder_name)
       predictor.model.tracking_proposals = setting['T']
       predictor.model.tracker.track_life = setting['track_life']
